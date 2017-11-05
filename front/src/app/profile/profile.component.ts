538e39ce42225a3e4080dcbd10df35a45c893a5b
areejmayadeh <engareej94@gmail.com>
2017-11-05 17:35:49 +0200

    add search for users and comment on profile

--- front/src/app/profile/profile.component.ts
+++ front/src/app/profile/profile.component.ts
@@ -19,22 +19,27 @@ export class ProfileComponent implements OnInit {
   postdata;
   profile;
   status;
+  //add this
+  comment;
+  comments;
+  id;
+  comID;
   constructor(private http : Http) { }
 
   ngOnInit() {
-   let headers = new Headers();
-   headers.append('Content-Type', 'application/json');
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
     return this.http.get('/profile',{headers: headers})
     .map((res) => {
       if(res){  
-         this.userData = res.json();
-         this.postdata = this.userData[0];
-         this.currentUser=this.userData[1][0].username;
-         this.nationality=this.userData[1][0].Nationallity;
-         this.location=this.userData[1][0].Location;
-         this.birthday = this.userData[1][0].Birthday;
-         this.profile=this.userData[1][0].imag;
-         this.status=this.userData[1][0].status;
+        this.userData = res.json();
+        this.postdata = this.userData[0];
+        this.currentUser=this.userData[1][0].username;
+        this.nationality=this.userData[1][0].Nationallity;
+        this.location=this.userData[1][0].Location;
+        this.birthday = this.userData[1][0].Birthday;
+        this.profile=this.userData[1][0].imag;
+        this.status=this.userData[1][0].status;
       }else{
         
       }
@@ -44,11 +49,29 @@ export class ProfileComponent implements OnInit {
   getStatus(){
     let set=$('#status').val();
     this.status=$('#status').val();
-      let headers = new Headers();
-      headers.append('Content-Type', 'application/json');
-      return this.http.put('/status',{status:set},{headers: headers})
-      .map((res) => {}).subscribe(); 
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.put('/status',{status:set},{headers: headers})
+    .map((res) => {}).subscribe(); 
 
+  }
+
+  commentFun(postIndex,roomId){
+    this.comment=$("#"+postIndex).val();
+    this.id=roomId;
+    this.comID=postIndex;
+    console.log(this.comment)
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('/postcomment',{commet:this.comment,roomid:this.id},{headers: headers})
+    .map((res) => {
+      if(res){
+        this.comments=res.json()
+
+      }else{
+
+      }
+    }).subscribe(); 
   }
 
 
