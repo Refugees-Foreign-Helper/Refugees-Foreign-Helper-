2ff93cfd6f0f10cddd70a51627a577a07800eca3
areejmayadeh <engareej94@gmail.com>
2017-11-05 19:36:00 +0200

    make secure

--- front/src/app/chat/chat.component.ts
+++ front/src/app/chat/chat.component.ts
@@ -49,7 +49,7 @@ let x=this;
   let $currentInput = $usernameInput.focus();
 
   // let socket = io();
- let socket = io.connect('http://localhost:3000');
+ let socket = io.connect('https://localhost:3000');
 
   function addParticipantsMessage (data) {
     let message = '';
